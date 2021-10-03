1b9fbdab0de1b79aab9457e87e388b0c25170a97
DharmendraSinghYadav <dharmendrasinghyadav022@gmail.com>
2021-10-03 23:16:02 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Todo from "./Todo";
-import TodoLists from "./TodoLists"
+
 
 ReactDOM.render(
   // <React.StrictMode>
